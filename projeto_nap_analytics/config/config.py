39f6bb42ff4f371da1252c80673926dff1d0871b
Thiago Gil Camargo <126835572+thiagogilcamargo@users.noreply.github.com>
2026-04-29 06:53:27 -0300

    Update config.py

--- projeto_nap_analytics/config/config.py
+++ projeto_nap_analytics/config/config.py
@@ -1,32 +1,6 @@
-# dashboard/config.py
+# config.py
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PATH_RAW = os.path.join(BASE_DIR, "dados", "raw", "dados.csv")
-
-# Nomes das colunas
-NECESSIDADE_COLS = [
-    "Já senti necessidade de apoio emocional durante a graduação.",
-    "Eu me sentiria confortável em procurar ajuda dentro da instituição.",
-    "Acredito que serviços de apoio podem melhorar a experiência acadêmica dos alunos."
-]
-
-SUPORTE_COLS = [
-    "Eu sinto que há suporte suficiente para dificuldades emocionais na faculdade."
-]
-
-INTENCAO_COLS = [
-    "Eu já pensei em utilizar o NAP (Núcleo de Apoio Psicopedagógico) em algum momento.",
-    "Tenho confiança na confidencialidade do atendimento oferecido pelo NAP (Núcleo de Apoio Psicopedagógico).",
-    "Eu sei como acessar os serviços oferecidos pelo NAP  (Núcleo de Apoio Psicopedagógico)."
-]
-
 JORNADA_COL = "Quais da opções abaixo melhor representa você em relação ao NAP (Núcleo de Apoio Psicopedagógico)?"
-
-ESFORCO_PROBLEMAS = {
-    "Falta de informação": 2,
-    "Preconceito": 6,
-    "Acesso (horários/local)": 7,
-    "Tempo de espera": 8
-}
